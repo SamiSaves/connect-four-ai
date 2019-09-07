cc4739a3c644c0f4d63f20aa2657b1c8aab33253
= <sami_eklund94@hotmail.com>
2019-09-07 13:30:45 +0300

    Add graphql schema and endpoints

---
+++ src/server/graphql/schema.js
@@ -0,0 +1,8 @@
+const { buildASTSchema } = require('graphql')
+const gql = require('graphql-tag')
+const { readFileSync } = require('fs')
+
+const graphqlSchema = readFileSync(__dirname + '/schema.graphql', 'utf8')
+const schema = buildASTSchema(gql(graphqlSchema))
+
+module.exports = { schema }
--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,28 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
-const { buildASTSchema } = require('graphql')
-const gql = require('graphql-tag')
 const cors = require('cors')
+const { schema } = require('./graphql/schema')
 
 const app = express()
 app.use(cors())
 
-const schema = buildASTSchema(gql`
-    type Query {
-        hello: String
-    }
-`)
-
 const rootValue = {
-    hello: () => 'Hello world!',
-    
+    createAiScript: ({ name, script }) => {
+        console.log('Creating script: ', name, script)
+        return { name, script, _id: 'newid' }
+    },
+    updateAiScript: ({ id, script }) => {
+        console.log('Updating script: ', script)
+        return { script, name: 'test123', _id: id }
+    },
+    createGame: ({ name }) => {
+        console.log('Creating a game', name)
+        return { name, _id: 'testid', pieces: [] }
+    },
+    insertPiece: ({ id, column, color }) => {
+        console.log('Inserting a piece', color, column)
+        return { name: 'testGame', _id: id, pieces: [ { position: { col: column, row: 0 }, color } ] }
+    }
 }
 
 app.use('/graphql', graphqlHTTP({ schema, rootValue, graphiql: true }))
