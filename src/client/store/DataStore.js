4c3d379c9310f129b1368299baf58d9ec5f54f40
= <sami_eklund94@hotmail.com>
2019-09-08 18:17:12 +0300

    Add getGame to data store

--- src/client/store/DataStore.js
+++ src/client/store/DataStore.js
@@ -1,7 +1,9 @@
 import { observable, action } from 'mobx'
 
 export default class DataStore {
-  @observable games = [];
+  @observable games = []
+
+  @observable currentGame
 
   @action
   getGames = async () => {
@@ -28,5 +30,38 @@ export default class DataStore {
     const games = await result.json()
 
     this.games = games.data.getGames
+  }
+
+  @action
+  getGame = async (id) => {
+    const query = `
+    query getGame($id: ID) {
+      getGame(id: $id){
+        _id
+        name
+        winner
+        currentTurn
+        pieces {
+          color
+          position {
+            column
+            row
+          }
+        }
+      }
+    }
+      `
+
+    const result = await fetch('http://localhost:3000/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({ query, variables: { id } }),
+    })
+
+    const game = await result.json()
+    this.currentGame = game.data.getGame
   }
 }
