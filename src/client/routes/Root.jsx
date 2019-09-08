1b2ac3021a2c0b99869d3607e1a0f53403c7af46
= <sami_eklund94@hotmail.com>
2019-09-08 16:46:03 +0300

    Add styles to content area

--- src/client/routes/Root.jsx
+++ src/client/routes/Root.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Switch, Route } from 'react-router-dom'
+import Paper from '@material-ui/core/Paper'
+import { withStyles } from '@material-ui/core/styles'
 import GamesList from './gamesList'
 import Navigation from './common/navigation'
 
-const Root = () => (
+const Root = (props) => (
   <div>
     <Navigation />
-    <div>
+    <Paper className={props.classes.contentArea}>
       <Switch>
         <Route exact path="/" component={GamesList} />
       </Switch>
-    </div>
+    </Paper>
   </div>
 )
 
-export default Root
+Root.propTypes = {
+  classes: PropTypes.object,
+}
+
+const styles = {
+  contentArea: {
+    marginTop: '70px',
+    padding: '20px',
+  },
+}
+
+export default withStyles(styles)(Root)
